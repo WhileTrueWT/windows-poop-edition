cc3695d7c0174380588a4b2e19cab05e8a7dd702
viba <viba@vibacomp.viba>
2022-10-13 12:03:14 -0600

    gui positioning + buttons

--- src/src/lib/wpgui.lua
+++ src/src/lib/wpgui.lua
@@ -14,15 +14,19 @@ end
 m.Gui = Object:extend()
 
 function m.Gui:new(t)
-    self.frame = t.frame or m.Frame()
+    self.frame = t.frame or m.Frame{width = t.width, height = t.height}
 end
 
 function m.Gui:put(...)
     self.frame:put(...)
 end
 
+function m.Gui:mousepressed(...)
+    self.frame:mousepressed(...)
+end
+
 function m.Gui:draw()
-    self.frame:draw()
+    self.frame:draw(0, 0)
 end
 
 -- Frame
@@ -30,6 +34,8 @@ end
 m.Frame = Object:extend()
 
 function m.Frame:new(t)
+    self.width = t.width or 100
+    self.height = t.height or 100
     self.content = {}
 end
 
@@ -39,18 +45,84 @@ function m.Frame:put(elements, params)
     local t = {}
     t.elements = elements
     t.align = params.align or "left"
+    t.verticalAlign = params.verticalAlign or "center"
     
     table.insert(self.content, t)
+    self:computePositions()
 end
 
-function m.Frame:draw()
+function m.Frame:computePositions()
+    local ex, ey = 0, 0
+    
     for _, group in ipairs(self.content) do
+        local totalWidth = 0
+        local totalHeight = 0
+        
+        for _, element in ipairs(group.elements) do
+            totalWidth = totalWidth + element.width + element.marginX*2
+            
+            local height = element.height + element.marginY*2
+            if height > totalHeight then
+                totalHeight = height
+            end
+        end
+        
+        if group.align == "left" then
+        elseif group.align == "right" then
+            ex = ex + self.width - totalWidth
+        elseif group.align == "center" then
+            ex = ex + math.floor(self.width/2 - totalWidth/2), 0
+        end
+        
         for _, element in ipairs(group.elements) do
             if checkType(element, Element) then
-                element:draw(0, 0)
+                
+                local x, y = ex + element.marginX, ey + element.marginY
+                
+                if group.verticalAlign == "top" then
+                elseif group.verticalAlign == "center" then
+                    y = y + totalHeight/2 - element.height/2
+                end
+                
+                element.x = x
+                element.y = y
+                ex = ex + element.width + element.marginX*2
+            end
+        end
+        
+        ex = 0
+        ey = ey + totalHeight
+    end
+end
+
+function m.Frame:mousepressed(x, y, button)
+    local mx, my = x - windowX, y - windowY
+    for _, group in ipairs(self.content) do
+        for _, element in ipairs(group.elements) do
+            print(mx, my, element.x, element.y, element.width, element.height)
+            if  (element.action) and (button == 1)
+            and element.x <= mx and mx <= element.x + element.width
+            and element.y <= my and my <= element.y + element.height
+            then
+                element:action()
             end
         end
     end
+end
+
+function m.Frame:draw(x, y)
+    love.graphics.push()
+    love.graphics.translate(x, y)
+    
+    for _, group in ipairs(self.content) do
+        for _, element in ipairs(group.elements) do
+            if checkType(element, Element) then
+                element:draw()
+            end
+        end
+    end
+    
+    love.graphics.pop()
 end
 
 -- Element
@@ -58,9 +130,15 @@ end
 Element = Object:extend()
 
 function Element:new(t)
+    self.x = 0
+    self.y = 0
     self.width = t.width or 0
     self.height = t.height or 0
+    self.marginX = t.marginX or 10
+    self.marginY = t.marginY or 10
 end
+
+function Element:draw() end
 
 -- Text
 
@@ -69,7 +147,7 @@ m.Text = Element:extend()
 function m.Text:new(t)
     self.text = t.text or ""
     self.font = t.font or love.graphics.getFont()
-    self.color = t.color or {0, 0, 0, 1}
+    self.color = t.color or style.text.color
     
     self.super.new(self, {
         width = t.width or self.font:getWidth(self.text),
@@ -77,10 +155,57 @@ function m.Text:new(t)
     })
 end
 
-function m.Text:draw(x, y)
+function m.Text:draw()
     love.graphics.setFont(self.font)
     love.graphics.setColor(self.color)
-    love.graphics.printf(self.text, x, y, self.width)
+    love.graphics.printf(self.text, self.x, self.y, self.width)
+end
+
+-- Button
+
+m.Button = Element:extend()
+
+function m.Button:new(t)
+    self.label = t.label or ""
+    self.action = t.action or function() end
+    self.color = t.color or style.button.color
+    self.tint = t.tint or {1, 1, 1, 1}
+    self.outlineColor = t.outlineColor or style.button.outlineColor
+    self.labelColor = t.labelColor or style.button.textColor
+    self.labelFont = t.labelFont or love.graphics:getFont()
+    
+    self.super.new(self, {
+        width = t.width or self.labelFont:getWidth(self.label) + 40,
+        height = t.height or 40
+    })
+end
+
+function m.Button:draw()
+    local brightnessOffset = 0
+    
+    local mx, my = love.graphics.inverseTransformPoint(love.mouse.getX(), love.mouse.getY())
+    if self.x <= mx and mx <= self.x + self.width and self.y <= my and my <= self.y + self.height then
+        brightnessOffset = 0.1
+    else
+        brightnessOffset = 0
+    end
+    
+    local newColor
+    
+    if type(self.color) == "string" then
+        if brightnessOffset > 0 then
+            newColor = {1, 1, 1, 0.25}
+        else
+            newColor = {0, 0, 0, 0}
+        end
+        image(self.color, self.x, self.y, self.width, self.height, self.tint)
+        rect(self.x, self.y, self.width, self.height, newColor)
+    else
+        newColor = {self.color[1] + brightnessOffset, self.color[2] + brightnessOffset, self.color[3] + brightnessOffset, self.color[4]}
+        rect(self.x, self.y, self.width, self.height, newColor)
+    end
+    outline(self.x, self.y, self.width, self.height, self.outlineColor)
+    text(self.label, self.x + self.width/2 - self.labelFont:getWidth(self.label)/2, self.y + self.height/2 - self.labelFont:getHeight()/2, self.labelColor)
 end
 
 return m
--- windows-src/test/main.lua
+++ windows-src/test/main.lua
@@ -5,15 +5,26 @@ local mainGui
 local window = {}
 
 function window.load()
-    mainGui = gui.Gui{}
+    mainGui = gui.Gui{width = windowWidth, height = windowHeight}
+    
+    local text = gui.Text{text = "hello"}
+    local button = gui.Button{label = "click", action = function()
+        sound("sounds/default.wav")
+        text.text = "yeah"
+    end}
     
     mainGui:put({
-        gui.Text{text = "hello"},
-    })
+        text,
+        button,
+    }, {align = "center"})
 end
 
 function window.draw()
     mainGui:draw()
+end
+
+function window.mousepressed(...)
+    mainGui:mousepressed(...)
 end
 
 return window
