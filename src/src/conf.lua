d0f414bc85abbd298728930833ebcd6983214e76
viba <viba@vibacomp.viba>
2022-10-16 18:39:04 -0600

    add löve version to conf.lua

--- src/src/conf.lua
+++ src/src/conf.lua
@@ -4,6 +4,7 @@
 
 function love.conf(t)
     t.identity = "winpe4"
+    t.version = "11.4"
     t.window.title = "Windows Poop Edition 5"
     t.window.icon = "icon.png"
     t.window.fullscreen = true
